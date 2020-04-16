164341df35abeca0c388b895d60ad6c630acf70a
Majid Abarghooei <abarghooeimajid@gmail.com>
2020-04-17 02:39:59 +0430

    Add Array implementation

---
+++ src/main/java/dev/coderator/algopractice/impl/Array.java
@@ -0,0 +1,51 @@
+package dev.coderator.algopractice.impl;
+
+public class Array {
+
+	private int count;
+	private int[] items;
+
+	public Array(int size) {
+		items = new int[size];
+	}
+
+	public void insert(int number) {
+		if (items.length == count)
+			reconstruct();
+
+		items[count++] = number;
+	}
+
+	private void reconstruct() {
+		int[] tmpArray = new int[items.length * 2];
+		for (int i = 0; i < items.length; i++)
+			tmpArray[i] = items[i];
+
+		items = tmpArray;
+	}
+
+	public int indexOf(int number) {
+		for (int i = 0; i < items.length; i++)
+			if (items[i] == number)
+				return i;
+
+		throw new RuntimeException(String.format("item not found: %d", number));
+	}
+
+	public void removeAt(int index) {
+		if (index >= count || index < 0)
+			throw new IndexOutOfBoundsException(String.format("index %d is out of range!", index));
+
+		for (int i = index; i < items.length - 1; i++)
+			items[i] = items[i + 1];
+		count--;
+	}
+
+	public int[] items() {
+		int[] tmpArray = new int[count];
+		for (int i = 0; i < count; i++)
+			tmpArray[i] = items[i];
+
+		return tmpArray;
+	}
+}
