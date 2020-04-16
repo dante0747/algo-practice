ef36cb5cc18feb0cef8946cc131a24605c08d595
Majid Abarghooei <abarghooeimajid@gmail.com>
2020-04-17 02:41:05 +0430

    Add Array implementation tests using Junit 5

--- src/main/java/dev/coderator/algopractice/impl/ArrayTest.java
+++ src/main/java/dev/coderator/algopractice/impl/ArrayTest.java
@@ -1,5 +1,66 @@
+package dev.coderator.algopractice.impl;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayTest {
 
+	@Test()
+	void insert() {
+
+		Array array = sampleArray();
+
+		int[] items = array.items();
+		assertEquals(5, items.length);
+		assertEquals("[5, 1, 6, 9, 0]", Arrays.toString(items));
+	}
+
+	@Test
+	void indexOf() {
+
+		Array array = sampleArray();
+
+		assertEquals(3, array.indexOf(9));
+		assertEquals(4, array.indexOf(0));
+		assertEquals(2, array.indexOf(6));
+
+		RuntimeException ex = assertThrows(RuntimeException.class, () -> array.indexOf(10));
+		assertEquals("item not found: 10", ex.getMessage());
+	}
+
+	@Test
+	void removeAt() {
+		Array array = sampleArray();
+
+		array.removeAt(4);
+		assertEquals("[5, 1, 6, 9]", Arrays.toString(array.items()));
+
+		array.removeAt(0);
+		assertEquals("[1, 6, 9]", Arrays.toString(array.items()));
+
+		array.removeAt(1);
+		assertEquals("[1, 9]", Arrays.toString(array.items()));
+
+		array.removeAt(0);
+		assertEquals("[9]", Arrays.toString(array.items()));
+
+		array.removeAt(0);
+		assertEquals("[]", Arrays.toString(array.items()));
+
+		IndexOutOfBoundsException ex = assertThrows(IndexOutOfBoundsException.class, () -> array.removeAt(10));
+		assertEquals("index 10 is out of range!", ex.getMessage());
+	}
+
+	private Array sampleArray() {
+		Array array = new Array(3);
+		array.insert(5);
+		array.insert(1);
+		array.insert(6);
+		array.insert(9);
+		array.insert(0);
+		return array;
+	}
 }
