5c5cb041fb78aa6846ac2039e09a16478dd671fa
Majid Abarghooei <abarghooeimajid@gmail.com>
2020-04-17 17:31:12 +0430

    Add linked list implementation and its corresponding tests

---
+++ src/main/java/dev/coderator/algopractice/datastructure/LinkedList.java
@@ -0,0 +1,136 @@
+package dev.coderator.algopractice.datastructure;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class LinkedList {
+
+	private class Node {
+		private int value;
+		private Node next;
+
+		public Node(int value) {
+			this.value = value;
+		}
+	}
+
+	private Node first;
+	private Node last;
+	private int size;
+
+	public void addFirst(int item) {
+		Node node = new Node(item);
+
+		if (isEmpty())
+			first = last = node;
+		else {
+			node.next = first;
+			first = node;
+		}
+
+		size++;
+	}
+
+	public void addLast(int item) {
+		Node node = new Node(item);
+
+		if (isEmpty())
+			first = last = node;
+		else {
+			last.next = node;
+			last = node;
+		}
+
+		size++;
+	}
+
+	private boolean isEmpty() {
+		return Objects.isNull(first);
+	}
+
+	public int indexOf(int item) {
+		int index = 0;
+		Node current = first;
+
+		while (Objects.nonNull(current)) {
+			if (current.value == item)
+				return index;
+			current = current.next;
+			index++;
+		}
+
+		throw new NoSuchElementException(String.format("item not found: %d", item));
+	}
+
+	public boolean contains(int item) {
+		Node current = first;
+
+		while (Objects.nonNull(current)) {
+			if (current.value == item)
+				return true;
+			current = current.next;
+		}
+
+		return false;
+	}
+
+	public void removeFirst() {
+		if (isEmpty())
+			throw new NoSuchElementException("the linked list is empty!");
+
+		if (first == last)
+			first = last = null;
+		else {
+			Node tmpNode = first;
+			first = first.next;
+			tmpNode.next = null;
+		}
+
+		size--;
+	}
+
+	public void removeLast() {
+		if (isEmpty())
+			throw new NoSuchElementException("the linked list is empty!");
+
+		if (first == last)
+			first = last = null;
+		else {
+			Node previous = previous(last);
+			last = previous;
+			last.next = null;
+		}
+
+		size--;
+	}
+
+	private Node previous(Node node) {
+		Node previous = null;
+		Node current = first;
+
+		while (current != node) {
+			previous = current;
+			current = current.next;
+		}
+		return previous;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public int[] toArray() {
+		int[] array = new int[size];
+
+		int index = 0;
+		Node current = first;
+
+		while (current != null) {
+			array[index++] = current.value;
+			current = current.next;
+		}
+
+		return array;
+	}
+}
+
---
+++ src/test/java/dev/coderator/algopractice/datastructure/LinkedListTest.java
@@ -0,0 +1,127 @@
+package dev.coderator.algopractice.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+
+	@Test
+	void addFirst() {
+		LinkedList list = new LinkedList();
+
+		list.addFirst(5);
+		list.addFirst(2);
+		list.addFirst(1);
+		list.addFirst(0);
+		list.addFirst(9);
+		assertEquals("[9, 0, 1, 2, 5]", Arrays.toString(list.toArray()));
+	}
+
+	@Test
+	void addLast() {
+		LinkedList list = new LinkedList();
+
+		list.addLast(5);
+		list.addLast(2);
+		list.addLast(1);
+		list.addLast(0);
+		list.addLast(9);
+		assertEquals("[5, 2, 1, 0, 9]", Arrays.toString(list.toArray()));
+	}
+
+	@Test
+	void indexOf() {
+		LinkedList list = list();
+		//[15, -6, 9, 1, 5, 2, 0, -1, 8]
+		assertEquals(0, list.indexOf(15));
+		assertEquals(3, list.indexOf(1));
+		assertEquals(7, list.indexOf(-1));
+		assertEquals(4, list.indexOf(5));
+		assertEquals(8, list.indexOf(8));
+
+		NoSuchElementException ex = assertThrows(NoSuchElementException.class, () -> list.indexOf(11));
+		assertEquals("item not found: 11", ex.getMessage());
+	}
+
+	@Test
+	void contains() {
+		LinkedList list = list();
+		assertEquals(true, list.contains(9));
+		assertEquals(false, list.contains(6));
+		assertEquals(true, list.contains(8));
+		assertEquals(true, list.contains(2));
+		assertEquals(false, list.contains(3));
+	}
+
+	@Test
+	void removeFirst() {
+		LinkedList list = list();
+		//[15, -6, 9, 1, 5, 2, 0, -1, 8]
+		for (int i = 0; i < 5; i++)
+			list.removeFirst();
+		assertEquals("[2, 0, -1, 8]", Arrays.toString(list.toArray()));
+
+		for (int i = 0; i < 3; i++)
+			list.removeFirst();
+		assertEquals("[8]", Arrays.toString(list.toArray()));
+
+		list.removeFirst();
+		assertEquals("[]", Arrays.toString(list.toArray()));
+
+		NoSuchElementException ex = assertThrows(NoSuchElementException.class, () -> list.removeFirst());
+		assertEquals("the linked list is empty!", ex.getMessage());
+	}
+
+	@Test
+	void removeLast() {
+		LinkedList list = list();
+		//[15, -6, 9, 1, 5, 2, 0, -1, 8]
+		for (int i = 0; i < 5; i++)
+			list.removeLast();
+		assertEquals("[15, -6, 9, 1]", Arrays.toString(list.toArray()));
+
+		for (int i = 0; i < 3; i++)
+			list.removeLast();
+		assertEquals("[15]", Arrays.toString(list.toArray()));
+
+		list.removeLast();
+		assertEquals("[]", Arrays.toString(list.toArray()));
+
+		NoSuchElementException ex = assertThrows(NoSuchElementException.class, () -> list.removeLast());
+		assertEquals("the linked list is empty!", ex.getMessage());
+	}
+
+	@Test
+	void size() {
+		LinkedList list = list();
+		//[15, -6, 9, 1, 5, 2, 0, -1, 8]
+		assertEquals(9, list.size());
+	}
+
+	@Test
+	void toArray() {
+		assertEquals("[15, -6, 9, 1, 5, 2, 0, -1, 8]", Arrays.toString(list().toArray()));
+	}
+
+	private LinkedList list() {
+
+		LinkedList list = new LinkedList();
+
+		list.addFirst(5); //[5]
+		list.addLast(2); //[5, 2]
+		list.addFirst(1); //[1, 5, 2]
+		list.addLast(0); //[1, 5, 2, 0]
+		list.addFirst(9); //[9, 1, 5, 2, 0]
+		list.addLast(-1); //[9, 1, 5, 2, 0, -1]
+		list.addFirst(-6); //[-6, 9, 1, 5, 2, 0, -1]
+		list.addLast(8); //[-6, 9, 1, 5, 2, 0, -1, 8]
+		list.addFirst(15); //[15, -6, 9, 1, 5, 2, 0, -1, 8]
+
+		return list;
+	}
+}
