b9cbc03e35c910e02f926fc601213e86e9b09bf7
Majid Abarghooei <abarghooeimajid@gmail.com>
2020-05-29 19:03:28 +0430

    Add hash table implementation

---
+++ src/main/java/dev/coderator/algopractice/datastructure/HashTable.java
@@ -0,0 +1,72 @@
+package dev.coderator.algopractice.datastructure;
+
+import java.util.*;
+import java.util.LinkedList;
+
+public class HashTable {
+
+	private class Entry {
+		private int key;
+		private String value;
+
+		public Entry(int key, String value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public String toString() {
+			return "(" + key + ", " + value + ")";
+		}
+	}
+
+	private LinkedList<Entry>[] buckets;
+
+	public HashTable(int size) {
+		buckets = new LinkedList[size];
+	}
+
+	public void put(int key, String value) {
+		var bucket = bucket(key);
+		entry(key).ifPresentOrElse(entry -> entry.value = value, () -> bucket.addLast(new Entry(key, value)));
+	}
+
+	public Optional<String> get(int key) {
+		return entry(key).map(entry -> entry.value);
+	}
+
+	public void remove(int key) {
+		entry(key).orElseThrow(() -> new IllegalStateException("entry not found"));
+		bucket(key).removeLast();
+	}
+
+	public List<Entry> entryList() {
+		var list = new ArrayList<Entry>();
+		for (var linkedList : buckets)
+			if (Objects.nonNull(linkedList))
+				linkedList.forEach(entry -> list.add(entry));
+
+		return list;
+	}
+
+	private int hash(int key) {
+		return key % buckets.length;
+	}
+
+	private LinkedList<Entry> bucket(int key) {
+		if (Objects.isNull(buckets[hash(key)]))
+			buckets[hash(key)] = new LinkedList<>();
+		return buckets[hash(key)];
+	}
+
+	private Optional<Entry> entry(int key) {
+		var bucket = bucket(key);
+		if (Objects.isNull(bucket))
+			return Optional.empty();
+
+		for (var entry : bucket)
+			if (entry.key == key)
+				return Optional.ofNullable(entry);
+
+		return Optional.empty();
+	}
+}
---
+++ src/test/java/dev/coderator/algopractice/datastructure/HashTableTest.java
@@ -0,0 +1,53 @@
+package dev.coderator.algopractice.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+	@Test
+	void put() {
+		assertEquals("[(10, Java), (20, Python), (1, C), (5, Ruby), (15, Go), (9, C++)]",
+				hashTable().entryList().toString());
+	}
+
+	@Test
+	void get() {
+		HashTable hashTable = hashTable();
+		assertEquals("Ruby", hashTable.get(5).get());
+		assertEquals("C++", hashTable.get(9).get());
+		assertEquals("Python", hashTable.get(20).get());
+		assertEquals(Optional.empty(), hashTable.get(25));
+	}
+
+	@Test
+	void remove() {
+		HashTable hashTable = hashTable();
+		hashTable.remove(9);
+		assertEquals("[(10, Java), (20, Python), (1, C), (5, Ruby), (15, Go)]", hashTable.entryList().toString());
+
+		hashTable.remove(15);
+		assertEquals("[(10, Java), (20, Python), (1, C), (5, Ruby)]", hashTable.entryList().toString());
+
+		hashTable.remove(5);
+		assertEquals("[(10, Java), (20, Python), (1, C)]", hashTable.entryList().toString());
+
+		assertThrows(IllegalStateException.class, () -> hashTable.remove(8));
+		assertThrows(IllegalStateException.class, () -> hashTable.remove(35));
+	}
+
+	private HashTable hashTable() {
+		HashTable hashTable = new HashTable(10);
+		hashTable.put(10, "Java");
+		hashTable.put(1, "C");
+		hashTable.put(9, "C++");
+		hashTable.put(20, "Python");
+		hashTable.put(5, "Ruby");
+		hashTable.put(15, "C#");
+		hashTable.put(15, "Go");
+		return hashTable;
+	}
+}
